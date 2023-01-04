822efb35e0bb16c630bd31322db2be02842a8665
WorkingBrain <94785988+WorkingBrain@users.noreply.github.com>
2023-01-04 16:45:16 +0530

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.get('/message', async (req, res) => {
 	const response = await fetch('https://discord.com/api/channels/1036555705366360117/messages', {
 		method: "POST",
 		headers: {
-			"Authorization": `${process.env.token}`,
+			"Authorization": `Bot ${process.env.token}`,
 			"Content-Type": "application/json"
 		},
 		body: JSON.stringify({
