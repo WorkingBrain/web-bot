b36043b5083d175ddfba10ac03c1ff8dbd941a5c
WorkingBrain <94785988+WorkingBrain@users.noreply.github.com>
2023-01-05 01:09:10 +0530

    Update index.js

--- index.js
+++ index.js
@@ -45,14 +45,50 @@ app.get('/message', async (req, res) => {
 	res.send('Sent the message')
 })
 
+app.get('/message', async (req, res) => {
+	const response = await fetch('https://discord.com/api/channels/1036555705366360117/messages', {
+		method: "POST",
+		headers: {
+			"Authorization": `Bot ${process.env.token}`,
+			"Content-Type": "application/json"
+		},
+		body: JSON.stringify({
+			"content": null,
+			"embeds": [
+				{
+					"description": "Click On the Button below to Gain Access To the Server",
+					"color": 3092790
+				}
+			],
+			"components": [
+				{
+					"type": 1,
+					"components": [
+						{
+							"type": 6,
+							"label": "Verify",
+							"style": 1,
+							"custom_id": "verify",
+							"emoji": {
+								"id": null,
+								"name": "🔐"
+							}
+						}
+					]
+				}
+			]
+		})
+	})
+	res.send('Sent the message')
+})
+
 app.post('/interactions', verifyKeyMiddleware(process.env.public_key), async (req, res) => {
 	const interaction = req.body
 	console.log(interaction)
 
 	if(interaction.type === 3) {
-		if (interaction.data.custom_id === `verify`) {
-			
-			await fetch(`https://discord.com/api/interactions/${interaction.id}/${interaction.token}/callback`, {
+
+		await fetch(`https://discord.com/api/interactions/${interaction.id}/${interaction.token}/callback`, {
 				method: "POST",
 				headers: {
 					"Authorization": `Bot ${process.env.token}`,
@@ -65,6 +101,8 @@ app.post('/interactions', verifyKeyMiddleware(process.env.public_key), async (re
 					}
 				})
 			})
+
+		if (interaction.data.custom_id === `verify`) {
 
 			await fetch(`https://discord.com/api/guilds/${interaction.guild_id}/members/${interaction.member.user.id}/roles/1060205348574724196`, {
 				method: "PUT",
@@ -84,8 +122,21 @@ app.post('/interactions', verifyKeyMiddleware(process.env.public_key), async (re
 				})
 			})
 			
-			res.sendStatus(200)
+		} else if (interaction.data.custom_id === `age`) {
+			var role
+
+			if(interaction.data.value === `adult`) {
+				role = 1060280489895788645
+			} else role = 1060280539732525159
+
+			await fetch(`https://discord.com/api/guilds/${interaction.guild_id}/members/${interaction.member.user.id}/roles/${role}`, {
+				method: "PUT",
+				headers: {
+					"Authorization": `Bot ${process.env.token}`
+				}
+			})
 		}
+		res.sendStatus(200)
 	}
 })
 
