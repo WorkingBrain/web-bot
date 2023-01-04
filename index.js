82ebf1f5808fa83f017522f7041fc06b94080215
WorkingBrain <94785988+WorkingBrain@users.noreply.github.com>
2023-01-04 16:00:00 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require('express')
+
+const app = express()
+
+app.get('/', (req, res) => {
+	res.sendStatus(200)
+})
+
+app.listen("3000", () => console.log(`listening on port 3000`))
