f41e3ae174d74e126ce5d77ea9c0addcdbba7d7f
WorkingBrain <94785988+WorkingBrain@users.noreply.github.com>
2023-01-05 15:45:37 +0530

    added role-update

---
+++ role-update.js
@@ -0,0 +1,45 @@
+async function role_update(interaction) {
+  const selected = interaction.data.values;
+  console.log(selected);
+
+  let options = [];
+
+  await interaction.message.components[0].components[0].options.forEach((element) =>
+    options.push(element.value)
+  );
+
+  const notselected = options.filter((option) => !selected.includes(option));
+  console.log(notselected);
+
+  if (notselected.length !== 0) {
+    notselected.forEach(async (role) => {
+      await fetch(
+        `https://discord.com/api/guilds/${interaction.guild_id}/members/${interaction.member.user.id}/roles/${role}`,
+        {
+          method: "DELETE",
+          headers: {
+            Authorization: `Bot ${process.env.token}`,
+          },
+        }
+      );
+    });
+  }
+
+  if (selected.length !== 0) {
+    selected.forEach(async (role) => {
+      await fetch(
+        `https://discord.com/api/guilds/${interaction.guild_id}/members/${interaction.member.user.id}/roles/${role}`,
+        {
+          method: "PUT",
+          headers: {
+            Authorization: `Bot ${process.env.token}`,
+          },
+        }
+      );
+    });
+  }
+
+  return "ok"
+}
+
+module.exports = { role_update }
