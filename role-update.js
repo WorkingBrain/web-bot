69b0e3ee93773429e92f7272136d607e07b5d6e3
WorkingBrain <94785988+WorkingBrain@users.noreply.github.com>
2023-01-05 16:26:04 +0530

    Update role-update.js

--- role-update.js
+++ role-update.js
@@ -4,8 +4,8 @@ async function role_update(interaction) {
 
   let options = [];
 
-  await interaction.message.components[0].components[0].options.forEach((element) =>
-    options.push(element.value)
+  await interaction.message.components[0].components[0].options.forEach(
+    (element) => options.push(element.value)
   );
 
   const notselected = options.filter((option) => !selected.includes(option));
@@ -13,33 +13,39 @@ async function role_update(interaction) {
 
   if (notselected.length !== 0) {
     notselected.forEach(async (role) => {
-      await fetch(
-        `https://discord.com/api/guilds/${interaction.guild_id}/members/${interaction.member.user.id}/roles/${role}`,
-        {
-          method: "DELETE",
-          headers: {
-            Authorization: `Bot ${process.env.token}`,
-          },
-        }
-      );
+      const timeout = setTimeout(async () => {
+        await fetch(
+          `https://discord.com/api/guilds/${interaction.guild_id}/members/${interaction.member.user.id}/roles/${role}`,
+          {
+            method: "DELETE",
+            headers: {
+              Authorization: `Bot ${process.env.token}`,
+            },
+          }
+        );
+        clearTimeout(timeout);
+      }, 2000);
     });
   }
 
   if (selected.length !== 0) {
     selected.forEach(async (role) => {
-      await fetch(
-        `https://discord.com/api/guilds/${interaction.guild_id}/members/${interaction.member.user.id}/roles/${role}`,
-        {
-          method: "PUT",
-          headers: {
-            Authorization: `Bot ${process.env.token}`,
-          },
-        }
-      );
+      const timer = setTimeout(async () => {
+        await fetch(
+          `https://discord.com/api/guilds/${interaction.guild_id}/members/${interaction.member.user.id}/roles/${role}`,
+          {
+            method: "PUT",
+            headers: {
+              Authorization: `Bot ${process.env.token}`,
+            },
+          }
+        );
+        clearTimeout(timer);
+      }, 2000);
     });
   }
 
-  return "ok"
+  return "ok";
 }
 
-module.exports = { role_update }
+module.exports = { role_update };
