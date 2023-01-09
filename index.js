54f275af6a1bb2547cef0e81d1a3c87978796609
WorkingBrain <94785988+WorkingBrain@users.noreply.github.com>
2023-01-10 01:37:39 +0530

    Update index.js

--- index.js
+++ index.js
@@ -99,7 +99,7 @@ app.post('/interactions', verifyKeyMiddleware(process.env.public_key), async (re
 					content: `Welcome <@${interaction.member.user.id}>.`,
 					embeds: [
 						{
-							description: `Daily Intraday setups : <#1053298075994697789>\nLearn stock market - <#1011953745740963890>\nSwing Trades - <#1011655226744381470>\nMarket Analysis - <#1053298242948976681>`,
+							description: `***```diff\n+Daily Intraday Setups :\n```*** **<#1053298075994697789>**\n\n***```diff\n-Learn Stock Market:\n```*** **<#1062095191093288990>**\n\n***```diff\n+Swing Trades:\n```*** **<#1062095347117199511>**\n\n***```diff\n-Market Analysis:\n```*** **<#1062095347117199511>**`,
 							color: 3092790
 						}
 					]
